71b967b97772de0239f8d01a5d7b496c4885a9d4
Mohd. Asfaq-E-Azam Rifat <rifat@w3engineers.com>
2018-12-28 14:57:47 +0600

    Learn arithmetic operators

---
+++ ArithmeticOperators.py
@@ -0,0 +1,26 @@
+age1 = 12
+age2 = 18
+
+print(age1 + age2)
+print(age1 - age2)
+print(age1 * age2)
+print(age1 / age2)
+print(age1 % age2)
+print(age2 % age1)
+
+sentence1 = "It was a beautiful day"
+
+firstName = "Mohd. Asfaq-E-Azam"
+lastName = "Rifat"
+
+print(firstName + " " + lastName)
+print(lastName * 2)
+
+# Can not use subtraction
+# Can not use division
+# Can not use modulus
+
+sentence2 = "Rifat is a programmer"
+print(sentence2[0:5])
+print(sentence2[:3])
+print(sentence2[:-10])
