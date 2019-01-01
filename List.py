f379989cdcd4606342932396c515bd8af6b0d76e
Mohd. Asfaq-E-Azam Rifat <rifat@w3engineers.com>
2019-01-01 11:32:27 +0600

    Learn list

---
+++ List.py
@@ -0,0 +1,19 @@
+fruitList = ["Mango", "Jackfruit", "Banana", "Orange", "Apple"]
+print(fruitList)
+print(fruitList[0])
+print(fruitList[0:2])
+
+fruitList.append("Cherry")
+print(fruitList)
+
+fruitList[0] = "Guava"
+print(fruitList)
+
+fruitList.remove("Guava")
+print(fruitList)
+
+print(fruitList * 2)
+
+numberList = [7, 13, 159, 15913]
+print(min(numberList))
+print(max(numberList))
