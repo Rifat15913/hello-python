e2c7319c306a80e584c8e5d9e2c6fc0964548b50
Mohd. Asfaq-E-Azam Rifat <rifat@w3engineers.com>
2019-01-03 12:27:03 +0600

    1. Learn try except 2. Learn comments

---
+++ basic/Comments.py
@@ -0,0 +1,7 @@
+# This is a single line comment
+
+"""
+This is multiple line comment
+Now you know it too
+Great!
+"""
---
+++ basic/TryExcept.py
@@ -0,0 +1,8 @@
+age = "Random Value"
+
+try:
+    if age < 13:
+        print("you are young")
+
+except:
+    print("Error occurred")
