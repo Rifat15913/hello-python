62e2eae97af0cf24c773a4c5655b3005811c2740
Mohd. Asfaq-E-Azam Rifat <rifat@w3engineers.com>
2019-01-03 11:02:46 +0600

    Learn conditional statements

---
+++ basic/ConditionalStatements.py
@@ -0,0 +1,13 @@
+age = 20
+
+if (age <= 19 and age >= 13):
+    print("You are teenager")
+elif (age < 13):
+    print("You are too young")
+else:
+    print("You are adult")
+
+# and operator
+# or operator
+# not operator
+# <, >, <=, >=, !=, == operators
